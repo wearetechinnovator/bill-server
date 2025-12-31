467d7df08b523cec8e8356df3df18f0c568ddd16
Sourav Bishai <souravbishai58558@gmail.com>
2025-12-31 13:08:24 +0530

    major changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(cors())
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 // app.use(morgan('tiny'))
 
 // const logStream = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" });
