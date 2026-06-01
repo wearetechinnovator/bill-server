986434be26c27ccd41910f07302a0ef145ece016
Sourav Bishai <106469372+techCodeSB@users.noreply.github.com>
2026-06-01 12:36:27 +0530

    changes role enum

--- models/user.model.js
+++ models/user.model.js
@@ -41,7 +41,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['admin', 'manager', 'user', 'accountant'],
+    enum: ['admin', 'manager', 'sales', 'accountant'],
     default: 'admin'
   },
   isDisable: {
